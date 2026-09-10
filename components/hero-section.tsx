272f69ba525707f8251b0653e9397eb0d2b25981
dolfito <b7c6kt27rm@privaterelay.appleid.com>
2026-09-10 20:37:35 +0200

    Actualizar hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -132,7 +132,7 @@ export const HeroSection = forwardRef<HTMLInputElement, HeroSectionProps>(functi
               <Globe className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
             )}
         
-135    <Input
+    <Input
   ref={ref}
   placeholder="Tu usuario"
   defaultValue=""
