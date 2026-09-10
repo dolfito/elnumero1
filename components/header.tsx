607144001fd63b17956ac5ece278323c7b2af655
dolfito <b7c6kt27rm@privaterelay.appleid.com>
2026-09-10 05:41:32 +0200

    Actualizar header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -25,7 +25,7 @@ export function Header() {
             </Button>
           )}
           <Link href="/" className="font-bold text-xl">
-            outbid.lol
+            Elnumero1
           </Link>
         </div>
         <nav className={cn('flex items-center gap-4', isMobile && 'hidden')}>
